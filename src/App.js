732e079cd3d629deeec8340c0169c4c5966c0468
ahsanahmedkhan <ahsanahmed.khan@hotmail.com>
2021-10-26 22:52:10 +0500

    adding navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Button } from "@material-ui/core";
+
 
 function App(){
+    
     return(
         <div>
-            <h1>Hello</h1>
+            <h1>3brother`s Enterprise</h1>
+            <Button
+             variant="outlined"
+             color="secondary" >Material UI</Button>
         </div>
     );
 }
